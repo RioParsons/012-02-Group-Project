3489b0db01fecbe81cc323acfaf42ef7e5cc10ea
Ethan <ethanansburgstda@gmail.com>
2023-04-12 15:55:50 -0600

    working on insert.sql

--- All Project code-components/init_data/create.sql
+++ All Project code-components/init_data/create.sql
@@ -1,29 +1,32 @@
+DROP DATABASE IF EXISTS restaurants_db;
+CREATE DATABASE restaurants_db;	
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-	user_id int SERIAL NOT NULL PRIMARY KEY,
+	user_id SERIAL NOT NULL PRIMARY KEY,
 	display_name VARCHAR(32),
     username VARCHAR(64) NOT NULL,
-    password CHAR(256) NOT NULL,
+    password CHAR(256) NOT NULL
 );
+
 
 DROP TABLE IF EXISTS owners CASCADE;
 CREATE TABLE owners(
-	owner_id int REFERENCES users(user_id),
+	owner_id INTEGER REFERENCES users(user_id)
 );
 
 DROP TABLE IF EXISTS restaurants CASCADE;
 CREATE TABLE restaurants(
-	restaurant_id int SERIAL NOT NULL PRIMARY KEY,
+	restaurant_id SERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	image_url VARCHAR(1024),
-	owner_id int REFERENCES owners(owner_id)
+	owner_id INTEGER REFERENCES users(user_id)
 );
 
 DROP TABLE IF EXISTS ratings CASCADE;
 CREATE TABLE ratings(
-	rating_id int SERIAL NOT NULL PRIMARY KEY,
-	restaurant_id int REFERENCES restaurants(restaurant_id) NOT NULL,
-	user_id int REFERENCES users(user_id) NOT NULL,
+	rating_id SERIAL NOT NULL PRIMARY KEY,
+	restaurant_id INTEGER REFERENCES restaurants(restaurant_id) NOT NULL,
+	user_id INTEGER REFERENCES users(user_id) NOT NULL,
 	last_updated TIMESTAMP NOT NULL,
 	uploaded TIMESTAMP NOT NULL,
 	rating_number decimal,
---
+++ All Project code-components/init_data/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (user_id, display_name, username, password)
+VALUES 
+(0, 'admin', 'admin@funny.com', 'password'),
+(1, 'ethan', 'ethan@gmail.com', 'password123');
+(2, 'testing', 'testingtesting123@yahoo.com', 'unencrypted password');
+
+INSERT INTO owners (owner_id) VALUES 
+(0);
+
+INSERT INTO restaurants (restaurant_id, name, image_url, owner_id) VALUES
+(0, 'Admins cool empty place', 'nowhere.com', 0);
